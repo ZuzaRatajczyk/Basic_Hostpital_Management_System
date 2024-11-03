49d829c09c4a8f99e581f8bab3d8462154778be5
ZuzaRatajczyk <zuza.ratajczyk@gmail.com>
2024-11-03 20:27:58 +0100

    Changed test_init logic

--- tests/test_HmsDatabase.py
+++ tests/test_HmsDatabase.py
@@ -5,7 +5,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from HmsDatabase import HmsDatabase
 
 
@@ -14,11 +14,11 @@ class TestHmsDatabase:
     @staticmethod
     @patch("HmsDatabase.PatientManagement")
     @patch("HmsDatabase.HmsDatabase._create_tables_if_not_exists")
-    @patch("HmsDatabase.mysql.connector.connect")
-    @patch("HmsDatabase.getpass")
-    @patch("builtins.input")
-    def test_init(_, __, ___, mocked_create_tables, mocked_pm):
+    @patch("HmsDatabase.HmsDatabase._db_connection_at_startup")
+    def test_init(mocked_connection, mocked_create_tables, mocked_pm):
+        mocked_connection.return_value = MagicMock(), MagicMock()
         db_obj = HmsDatabase()
+        mocked_connection.assert_called_once()
         mocked_create_tables.assert_called_once()
         mocked_pm.called_once_with(db_obj.db, db_obj.db_cursor)
         assert mocked_pm.return_value == db_obj.patients_management
