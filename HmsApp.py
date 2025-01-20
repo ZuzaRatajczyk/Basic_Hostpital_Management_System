43a843499b900a9f664fb114e99945b5e79fc368
ZuzaRatajczyk <zuza.ratajczyk@gmail.com>
2025-01-20 22:29:17 +0100

    Preparation for next module addition

--- HmsApp.py
+++ HmsApp.py
@@ -36,29 +36,33 @@ class HmsApp:
                 print("Incorrect value. Please respond with 'yes' or 'no' (or 'y' or 'n')")
 
     def choose_action(self):
-        application_modules = {1: {"Patient Management": ["Register patient", "Find patient", "Edit patient"]}}
+        application_modules = {1: {"Patient Management": ["Register patient", "Find patient", "Edit patient"]},
+                               2: {"Doctor Management": ["Add doctor", "Find doctor", "Edit doctor"]}}
         print("Choose number of module:\n")
         for module_num in application_modules:
             for module_name in application_modules[module_num]:
                 print(module_num, module_name, sep=". ")
-        module_number = int(input())
-        if module_number == 1:
-            module_name = "Patient Management"
-            patient_management = self.HmsDb.patients_management
-            self.is_module_running = True
-            while self.is_module_running:
-                print("Choose number of action:")
-                for submodule_num, submodule in enumerate(application_modules[module_number][module_name], start=1):
-                    print(submodule_num, submodule, sep=". ")
-                user_action = int(input())
-                if user_action == 1:
-                    patient_management.register_patient()
-                    self.check_app_status(module_name=module_name)
-                if user_action == 2:
-                    patient_management.find_patient()
-                    self.check_app_status(module_name=module_name)
-                if user_action == 3:
-                    patient_management.edit_patient()
-                    self.check_app_status(module_name=module_name)
+        user_module_number = int(input())
+        module_name = ""
+        for module_num, module in application_modules.items():
+            if module_num == user_module_number:
+                module_name = [key for key in module.keys()][0]
+        patient_management = self.HmsDb.patients_management
+        self.is_module_running = True
+        while self.is_module_running:
+            print("Choose number of action:")
+            for submodule_num, submodule in enumerate(application_modules[user_module_number][module_name], start=1):
+                print(submodule_num, submodule, sep=". ")
+            user_action = int(input())
+            if user_action == 1:
+                patient_management.register_patient()
+                self.check_app_status(module_name=module_name)
+            if user_action == 2:
+                patient_management.find_patient()
+                self.check_app_status(module_name=module_name)
+            if user_action == 3:
+                patient_management.edit_patient()
+                self.check_app_status(module_name=module_name)
+
 
 
